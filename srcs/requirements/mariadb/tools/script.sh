bfbbf462171b7445d58e99fd04a348cea5f7127f
Alistair Kane <alkane@c2s3d6.42wolfsburg.de>
2023-06-08 19:20:46 +0200

    starting fresh, using alpine now instead of ubuntu

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-service mysql start
+echo "CREATE DATABASE IF NOT EXISTS $MARIA_NAME ;" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $MARIA_NAME.* TO '$MARIA_USER'@'%' IDENTIFIED BY '$MARIA_PASSWORD';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
-
-mysql < db1.sql
-
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
+mysqld --user=mysql --init-file=/tmp/init.sql
--- srcs/requirements/nginx/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=DE/L=Wolfsburg/O=42/OU=student/CN=[alkane.42.de](http://alkane.42.de/)"
-
-nginx -g daemon off;
