131ee292d348029cb0485222722bdfad426b9545
alistair kane <alistair.kane94@gmail.com>
2023-06-11 22:27:29 +0200

    most working version

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -12,12 +12,12 @@ sleep 5
 
 wp core download --allow-root --version=5.8.1 --locale=en_US
 
-wp config create --allow-root --dbname=${WP_NAME} --dbuser=${WP_ADMIN_USR} --dbpass=${WP_PASSWORD} --dbhost=${WP_HOST}
+wp config create --allow-root --dbname=${WP_NAME} --dbuser=${WP_ADMIN_USER} --dbpass=${WP_PASSWORD} --dbhost=${WP_HOST}
 
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
 
 # creates a new user account with the specified username, email address, and password. The --role option sets the user's role to author, which gives the user the ability to publish and manage their own posts.
-wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="{$WP_PASSWORD}" --role=author
+wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_PASSWORD}" --role=author
 
 # installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
 wp theme install astra --activate --allow-root
